69f28b89a37f630569ce96a9037dc560e302dbff
Raphael Pereira de Araujo <145080836+Phael7@users.noreply.github.com>
2024-06-24 16:35:43 -0300

    Repository - Final
    Jogo funcionando com javascript

---
+++ questions.js
@@ -0,0 +1,82 @@
+export default [
+    {
+      question: "Quem foi a primeira pessoa a viajar no Espaço?",
+      answers: [
+        { option: "Yuri Gagarin", correct: true },
+        { option: "A cadela Laika", correct: false },
+        { option: "Marcos Pontes", correct: false },
+      ],
+    },
+    {
+      question: "Qual a montanha mais alta do mundo?",
+      answers: [
+        { option: "Mauna Kea", correct: false },
+        { option: "Monte Everest", correct: true },
+        { option: "Monte Chimborazo", correct: false },
+      ],
+    },
+    {
+      question: "Onde se localiza Machu Picchu?",
+      answers: [
+        { option: "Colômbia", correct: false },
+        { option: "Peru", correct: true },
+        { option: "Índia", correct: false },
+      ],
+    },
+    {
+      question: "Quem inventou a lâmpada?",
+      answers: [
+        { option: "Steve Jobs", correct: false },
+        { option: "Thomas Edison", correct: true },
+        { option: "Graham Bell", correct: false },
+      ],
+    },
+    {
+        question: "Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?",
+        answers: [
+          { option: "365 dias", correct: false },
+          { option: "366 dias", correct: false },
+          { option: "Aproximadamente 24 horas", correct: true },
+        ],
+      },
+    {
+        question: "A que temperatura a água ferve?",
+        answers: [
+          { option: "200 ºC", correct: false },
+          { option: "0 ºC", correct: false },
+          { option: "100 ºC", correct: true },
+        ],
+    },
+    {
+        question: "Qual o maior planeta do sistema solar?",
+        answers: [
+          { option: "Marte", correct: false },
+          { option: "Júpiter", correct: true },
+          { option: "Terra", correct: false },
+        ],
+    },
+    {
+        question: "Qual o planeta mais próximo do Sol?",
+        answers: [
+          { option: "Mercúrio", correct: true },
+          { option: "Marte", correct: false },
+          { option: "Netuno", correct: false },
+        ],
+    },
+    {
+        question: "Quantos continentes existem?",
+        answers: [
+          { option: "8", correct: false },
+          { option: "4", correct: false },
+          { option: "6", correct: true },
+        ],
+    },
+    {
+        question: "Qual a maior floresta tropical do mundo?",
+        answers: [
+          { option: "Mata Atlântica", correct: false },
+          { option: "Pantanal", correct: false },
+          { option: "Floresta Amazônica", correct: true },
+        ],
+    },  
+  ];
